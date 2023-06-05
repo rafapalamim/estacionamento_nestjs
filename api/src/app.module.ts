38298bb240e7e0209f9751b399e09acf800f1f7f
Rafael Palamim <palamimweb@gmail.com>
2023-06-05 04:20:57 -0300

    Gerando resouce de veículo

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
 import { MySQLModule } from './database/modules/mysql.module';
+import { VeiculoModule } from './veiculo/veiculo.module';
 
 @Module({
-  imports: [MySQLModule, EstabelecimentoModule],
+  imports: [MySQLModule, EstabelecimentoModule, VeiculoModule],
   controllers: [],
   providers: [],
 })
